1431a8478290cd2fa03c1c7088d9d2eb695e3179
Chris Morris <chris.morris@crowncommercial.gov.uk>
2026-02-25 13:55:57 +0000

    update spec

--- e2e-tests/tests/create.latest.news.from.fixture.spec.ts
+++ e2e-tests/tests/create.latest.news.from.fixture.spec.ts
@@ -1,14 +1,16 @@
 import { test } from "../src/wp.fixtures";
 import Post from "../src/models/Post";
 import { loadFixture } from "../src/utils/loadFixture";
+import { decode } from "html-entities";
 
 test.describe("homepage - latest news", () => {
   test("should render a real production article on QA", async ({ wp, homepage }) => {
     const fixture = loadFixture("tests/data/latest-news-1.json");
 
     const post = Post.aPost()
-      .withFixedTitle(fixture.title)
-      .withContent(fixture.contentHtml) // IMPORTANT: keep HTML as-is
+      .withFixedTitle(decode(fixture.title))
+      .withContent(fixture.contentHtml)
+      .withFeaturedImage("featured-1-new.jpg")
       .withStatus("publish");
 
     await wp.posts.create(post);
