57ba72b8ba3b2d36d30dc768bdeae1f8371b773b
Chris Morris <morris27sky@gmail.com>
2026-03-04 00:00:11 +0000

    comment out unused tests

--- e2e-tests/tests/smoke.create.latest.news.from.fixture.spec.ts
+++ e2e-tests/tests/smoke.create.latest.news.from.fixture.spec.ts
@@ -1,21 +1,21 @@
-import { test } from "../src/wp.fixtures";
-import Post from "../src/models/Post";
-import { loadFixture } from "../src/utils/loadFixture";
-import { decode } from "html-entities";
+// import { test } from "../src/wp.fixtures";
+// import Post from "../src/models/Post";
+// import { loadFixture } from "../src/utils/loadFixture";
+// import { decode } from "html-entities";
 
-test.describe("homepage - latest news", () => {
-  test("should render a real production article on QA", async ({ wp, homepage }) => {
-    const fixture = loadFixture("tests/data/latest-news-1.json");
+// test.describe("homepage - latest news", () => {
+//   test.skip("should render a real production article on QA", async ({ wp, homepage }) => {
+//     const fixture = loadFixture("tests/data/latest-news-1.json");
 
-    const post = Post.aPost()
-      .withFixedTitle(decode(fixture.title))
-      .withContent(fixture.contentHtml)
-      .withFeaturedImage("featured-1-new.jpg")
-      .withStatus("publish");
+//     const post = Post.aPost()
+//       .withFixedTitle(decode(fixture.title))
+//       .withContent(fixture.contentHtml)
+//       .withFeaturedImage("featured-1-new.jpg")
+//       .withStatus("publish");
 
-    await wp.posts.create(post);
+//     await wp.posts.create(post);
 
-    await homepage.goto();
-    await homepage.assertLatestNewsLayout([post]);
-  });
-});
+//     await homepage.goto();
+//     await homepage.assertLatestNewsLayout([post]);
+//   });
+// });
