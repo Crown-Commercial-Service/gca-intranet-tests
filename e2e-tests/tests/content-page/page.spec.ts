93f4899be4e538dbfc8a627a0d3f4b4df2432745
Chris Morris <morris27sky@gmail.com>
2026-05-06 14:40:23 +0100

    add e2e ui tests to assert contact form on page

--- e2e-tests/tests/content-page/page.spec.ts
+++ e2e-tests/tests/content-page/page.spec.ts
@@ -40,6 +40,48 @@ test.describe("Content page component", () => {
     await contentPage.assertTwoColumnTemplateIsApplied();
   });
 
+  test(
+    "HR Directorate contact card displays on a published page",
+    { tag: "@regression" },
+    async ({ wp, wordpressLoginPage, contentPage, runId }) => {
+      const templatePage = Post.aPage()
+        .withFixedTitle(`Contact Card Test ${runId}`)
+        .withParagraphMaxChars(180)
+        .withStatus("publish");
+
+      const pageId = await wp.posts.create(templatePage);
+
+      await wordpressLoginPage.goto();
+      await wordpressLoginPage.loginAsAdmin();
+
+      await contentPage.gotoEdit(pageId);
+      await contentPage.selectTeam("HR Directorate");
+      await contentPage.update();
+
+      await contentPage.gotoById(pageId);
+
+      await contentPage.openContactCard();
+      await contentPage.assertContactCardTitle("HR Directorate");
+      await contentPage.assertContactCardDescription(
+        "Short description shown below the heading",
+      );
+      await contentPage.assertContactCardItemCount(3);
+      await contentPage.assertContactCardItem({
+        title: "Email",
+        subtitle: "hr-connect@gca.gov.uk",
+      });
+      await contentPage.assertContactCardItem({
+        title: "Contact",
+        subtitle: "08000000000",
+      });
+      await contentPage.assertContactCardItem({
+        title: "Link",
+        subtitle: "Example",
+        href: "http://www.example.com",
+      });
+    },
+  );
+
   test.skip("can add multiple page contents to a page", async ({
     wp,
     wordpressLoginPage,
