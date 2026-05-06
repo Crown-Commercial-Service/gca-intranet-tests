87401f5d86fa355bdd8f71141ddb61839754bfae
Chris Morris <morris27sky@gmail.com>
2026-05-06 14:41:18 +0100

    format spec

--- e2e-tests/tests/content-page/page.spec.ts
+++ e2e-tests/tests/content-page/page.spec.ts
@@ -10,35 +10,34 @@ test.describe("Content page component", () => {
     await wp.posts.clearByTypeAndAuthor("page");
   });
 
-  test("can create a 2 column template", { tag: "@regression" }, async ({
-    wp,
-    wordpressLoginPage,
-    contentPage,
-    runId,
-  }) => {
-    const templatePage = Post.aPage()
-      .withFixedTitle(`Two Column Template ${runId}`)
-      .withParagraphMaxChars(180)
-      .withFeaturedImage("featured.jpg")
-      .withStatus("publish");
+  test(
+    "can create a 2 column template",
+    { tag: "@regression" },
+    async ({ wp, wordpressLoginPage, contentPage, runId }) => {
+      const templatePage = Post.aPage()
+        .withFixedTitle(`Two Column Template ${runId}`)
+        .withParagraphMaxChars(180)
+        .withFeaturedImage("featured.jpg")
+        .withStatus("publish");
 
-    const pageId = await wp.posts.create(templatePage);
+      const pageId = await wp.posts.create(templatePage);
 
-    await wordpressLoginPage.goto();
-    await wordpressLoginPage.loginAsAdmin();
+      await wordpressLoginPage.goto();
+      await wordpressLoginPage.loginAsAdmin();
 
-    await contentPage.gotoEdit(pageId);
-    await contentPage.fillSlug(`two-column-template-${runId}`);
-    await contentPage.selectColumnTemplate("Layout - 2 column");
-    await contentPage.fillExcerpt(templatePage);
-    await contentPage.selectAudience("Line managers");
-    await contentPage.selectCategory("Digital and data");
-    await contentPage.selectContentType("Staff network");
-    await contentPage.selectTeam("Able Network");
-    await contentPage.update();
-    await contentPage.gotoById(pageId);
-    await contentPage.assertTwoColumnTemplateIsApplied();
-  });
+      await contentPage.gotoEdit(pageId);
+      await contentPage.fillSlug(`two-column-template-${runId}`);
+      await contentPage.selectColumnTemplate("Layout - 2 column");
+      await contentPage.fillExcerpt(templatePage);
+      await contentPage.selectAudience("Line managers");
+      await contentPage.selectCategory("Digital and data");
+      await contentPage.selectContentType("Staff network");
+      await contentPage.selectTeam("Able Network");
+      await contentPage.update();
+      await contentPage.gotoById(pageId);
+      await contentPage.assertTwoColumnTemplateIsApplied();
+    },
+  );
 
   test(
     "HR Directorate contact card displays on a published page",
@@ -99,12 +98,11 @@ test.describe("Content page component", () => {
     await wordpressLoginPage.goto();
     await wordpressLoginPage.loginAsAdmin();
 
-        await contentPage.gotoEdit(pageId);
+    await contentPage.gotoEdit(pageId);
 
     // await contentPage.pause()
-    await contentPage.addTextComponent('oishfodofjo');
+    await contentPage.addTextComponent("oishfodofjo");
 
     await contentPage.update();
-
   });
 });
